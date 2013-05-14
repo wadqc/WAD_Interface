3bde675ecb14a03aa0fb8b6278573aa9ed8207fa
Tim de Wit <t.c.dewit@amc.uva.nl>
2013-05-14 09:20:40 +0200

    Ralph: De controle op nieuwe studies duurt langer nagelang er meer studies in de db zitten
    De controle op nieuwe studies duurt langer nagelang er meer studies in
    de db zitten. Bij ongeveer 6000 studies duurt het ongeveer 2 minuten
    voor de Collector één cyclus te doorlopen. Ik heb de code aangepast
    waarbij gebruik gemaakt wordt van een sql-query om de tabbellen dcm4chee
    en iqc te vergelijken en alleen de verschillen als resultaat te geven
    (studies die wel in dcm4chee staan maar niet in iqc). Momenteel duurt
    één cyclus bij ongeveer 6000 studies 26 seconden.
    Gevolg is dat de db-gebruik van dcm4chee wel rechten moet hebben om
    select queries uit te voeren op de iqc database. Hiervoor is het script
    iqc_users.cmd aangepast. (Ralph)

--- create_databases/iqc_db/iqc_users.sql
+++ create_databases/iqc_db/iqc_users.sql
@@ -1,5 +1,8 @@
 -- Gebruiker aanmaken om vanuit de collector bewerkingen te kunnen doen (moet overeenkomen met de gebruiker in config.xml bij de WAD_Collector)
 grant all on iqc.* to 'wad'@'localhost' identified by 'wad';
 
+--Gebruiker rechten geven om select queries uit te voeren op de database van dcm4chee.
+grant select on pacsdb.* to 'wad'@'localhost' identified by 'wad';
+
 -- gebruikt in WAD_Interface
 grant all on iqc.* to 'iqc'@'localhost' identified by 'TY8BqYRdn3Uhzq8T';

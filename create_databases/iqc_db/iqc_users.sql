991cc2ad22ccc8d40af9af14cd8ed40ebc7452ca
rijkhorst <rijkhorst@gmail.com>
2013-05-20 13:56:30 +0300

    Update iqc_users.sql
    Regel 4: spatie miste na comment --Gebruiker rechten geven...

--- create_databases/iqc_db/iqc_users.sql
+++ create_databases/iqc_db/iqc_users.sql
@@ -1,7 +1,7 @@
 -- Gebruiker aanmaken om vanuit de collector bewerkingen te kunnen doen (moet overeenkomen met de gebruiker in config.xml bij de WAD_Collector)
 grant all on iqc.* to 'wad'@'localhost' identified by 'wad';
 
---Gebruiker rechten geven om select queries uit te voeren op de database van dcm4chee.
+-- Gebruiker rechten geven om select queries uit te voeren op de database van dcm4chee.
 grant select on pacsdb.* to 'wad'@'localhost' identified by 'wad';
 
 -- gebruikt in WAD_Interface

3218832fbc08bb66e4289d8869b50f08396f8e25
Joost Kuijer <jpa.kuijer@vumc.nl>
2013-08-26 14:32:23 +0200

    bugfix: oud menu bij niet-admin levels

--- website/WAD-IQC/database/iqc/menu_structure.php
+++ website/WAD-IQC/database/iqc/menu_structure.php
@@ -49,23 +49,32 @@ if (empty($user_level_1))
 //$level['top']['Admin']=1;
 $level['top']['Selector']=2;
 $level['top']['Results']=3;
+$level['top']['Status']=4;
 
 //$level['Admin']['Users']=1;
 //$action['Admin']['Users']='../iqc/create_users.php';
 
-$level['Selector']['Test Files']=1;
+$level['Selector']['Modules']=1;
 $level['Selector']['Config Files']=2;
 $level['Selector']['Selector']=3;
 
 
-$action['Selector']['Test Files']='../iqc/create_analysemodule.php';
+$action['Selector']['Modules']='../iqc/create_analysemodule.php';
 $action['Selector']['Config Files']='../iqc/create_analysemodule_cfg.php';
 $action['Selector']['Selector']='../iqc/create_selector.php';
+
 
 
 $level['Results']['Selector']=1;
 
 $action['Results']['Selector']='../iqc/show_selector.php';
+
+$level['Status']['Collector Selector']=1;
+$level['Status']['Processor']=2;
+
+
+$action['Status']['Collector Selector']='../iqc/status-collector.php';
+$action['Status']['Processor']='../iqc/status-processor.php';
 
 
 

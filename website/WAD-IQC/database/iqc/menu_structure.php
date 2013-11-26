5325fb032edd5ec42c153e802aa2cd62771a1bc6
Joost Kuijer <jpa.kuijer@vumc.nl>
2013-11-26 11:43:46 +0100

    Menu Col- Selector hernoemd Selector
    Naam "Col- Selector" slecht leesbaar, informatie in deze tabel doorgaans
    relevant bij check funtioneren selector.

--- website/WAD-IQC/database/iqc/menu_structure.php
+++ website/WAD-IQC/database/iqc/menu_structure.php
@@ -29,10 +29,10 @@ $level['Results']['Selector']=1;
 $action['Results']['Selector']='../iqc/show_selector.php';
 
 
-$level['Status']['Col- Selector']=1;
+$level['Status']['Selector']=1;
 $level['Status']['Processor']=2;
 
-$action['Status']['Col- Selector']='../iqc/status-collector.php';
+$action['Status']['Selector']='../iqc/status-collector.php';
 $action['Status']['Processor']='../iqc/status-processor.php';
 
 
@@ -66,10 +66,10 @@ $level['Results']['Selector']=1;
 $action['Results']['Selector']='../iqc/show_selector.php';
 
 
-$level['Status']['Col- Selector']=1;
+$level['Status']['Selector']=1;
 $level['Status']['Processor']=2;
 
-$action['Status']['Col- Selector']='../iqc/status-collector.php';
+$action['Status']['Selector']='../iqc/status-collector.php';
 $action['Status']['Processor']='../iqc/status-processor.php';
 
 }

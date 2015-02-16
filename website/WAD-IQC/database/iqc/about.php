5102905770acf5adba87dda87bdd523b64c208f6
T.C. de Wit <t.c.dewit@amc.uva.nl>
2015-02-16 21:17:53 +0100

    Fixt fout bij openen van de "about" pagina als de gebruiker niet is ingelogd. De versie-tabel wordt nu alleen getoond als de gebruiker is ingelogd.

--- website/WAD-IQC/database/iqc/about.php
+++ website/WAD-IQC/database/iqc/about.php
@@ -1,57 +1,67 @@
 <?php
 
-require("../globals.php") ;
 require("./common.php") ;
 require("./php/includes/setup.php");
 
-$table_config='config';
-
-$versions_Stmt="SELECT * from $table_config where property like 'Version_%'
-                order by field(property,'Version_Database','Version_Collector','Version_Selector','Version_Processor')";
-
-// Connect to the Database
-$link = new mysqli($hostName, $userName, $password, $databaseName);
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-$result_versions = $link->query($versions_Stmt);
-
 $table_versions='';
+$table_title='';
 
-$j=0;
-while (($field_config = $result_versions->fetch_object()))
-{
 
-   $b=($j%2);
-   $bgcolor='';
-   if ($b==0)
-   {
-     $bgcolor='#B8E7FF';
-   }
+if (!session_id())
+session_start();
 
-   $table_data = new Smarty_NM();
+// check of er al is ingelogd; anders hebben we geen db toegang
+if(!empty($_SESSION['user_name'])) {
 
-   if ($j==0) //define header data
-   {
-     $table_versions=$table_data->fetch("about_versions_header.tpl");
-   }
+  require("../globals.php") ;
 
-   $table_data->assign("bgcolor",$bgcolor);
-   $table_data->assign("software_onderdeel",str_replace('Version_','',$field_config->property));
-   $table_data->assign("software_versie",$field_config->value);
-   $table_data->assign("laatste_wijziging",$field_config->date_modified);
+  $table_config='config';
+  $table_title='Ge&iuml;nstalleerde versies van Java services en WAD-QC Database:';
 
-   $table_versions.=$table_data->fetch("about_versions_row.tpl");
+  $versions_Stmt="SELECT * from $table_config where property like 'Version_%'
+                  order by field(property,'Version_Database','Version_Collector','Version_Selector','Version_Processor')";
 
-   $j++;
+  // Connect to the Database
+  $link = new mysqli($hostName, $userName, $password, $databaseName);
+
+  /* check connection */
+  if (mysqli_connect_errno()) {
+      printf("Connect failed: %s\n", mysqli_connect_error());
+      exit();
+  }
+
+  $result_versions = $link->query($versions_Stmt);
+
+  $j=0;
+  while (($field_config = $result_versions->fetch_object()))
+  {
+
+     $b=($j%2);
+     $bgcolor='';
+     if ($b==0)
+     {
+       $bgcolor='#B8E7FF';
+     }
+
+     $table_data = new Smarty_NM();
+
+     if ($j==0) //define header data
+     {
+       $table_versions=$table_data->fetch("about_versions_header.tpl");
+     }
+
+     $table_data->assign("bgcolor",$bgcolor);
+     $table_data->assign("software_onderdeel",str_replace('Version_','',$field_config->property));
+     $table_data->assign("software_versie",$field_config->value);
+     $table_data->assign("laatste_wijziging",$field_config->date_modified);
+
+     $table_versions.=$table_data->fetch("about_versions_row.tpl");
+
+     $j++;
+  }
+
+  $result_versions->close();
 }
-
-$result_versions->close();
-
 
 
 
@@ -61,6 +71,7 @@ $main_logo=sprintf("./../../../WAD-logo_pictures/logo_iqc.jpg");
 
 $start_page->assign("main_logo",$main_logo);
 $start_page->assign("versions_list",$table_versions);
+$start_page->assign("versions_title",$table_title);
 
 $start_page->display("wad_about.tpl");
 
